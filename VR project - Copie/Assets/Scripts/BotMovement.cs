2246be40da38a5df4dd185262906550ae84ad7cc
Alexis Soltysiak <65892122+nobodyonfire@users.noreply.github.com>
2021-02-16 14:27:06 +0100

    uptdate bot movement

--- VR project - Copie/Assets/Scripts/BotMovement.cs
+++ VR project - Copie/Assets/Scripts/BotMovement.cs
@@ -1,7 +1,8 @@
 using UnityEngine;
 public class BotMovement : MonoBehaviour
 {
-    [SerializeField] private float movement;
+    [SerializeField] private float movementx;
+    [SerializeField] private float movementz;
     private float timer;
     private float orientation;
     public float framerate;
@@ -9,13 +10,20 @@ public class BotMovement : MonoBehaviour
     private Transform PlayerTransform;
     [SerializeField] private GameObject player;
     private Vector3 PlayerAngle;
+    private Vector3 PlayerMovement;
+    
     void Start()
     {
         timer = 0.0f;
         Vector3 playerPosition = transform.position;
-        movement = 0.05f;
         PlayerAngle = transform.rotation.eulerAngles;
         PlayerTransform = player.GetComponent<Transform>();
+        orientation = -1;
+        if (movementx != 0)
+        {
+            PlayerAngle = new Vector3(PlayerAngle.x, PlayerAngle.y +90, PlayerAngle.z);
+        }
+        
 
     }
     private void Update()
@@ -26,11 +34,25 @@ public class BotMovement : MonoBehaviour
         
         if (timer >= framerate)
         {
-            PlayerAngle = new Vector3(PlayerAngle.x, PlayerAngle.y + 180, PlayerAngle.z);
-            PlayerTransform.rotation = Quaternion.Euler(PlayerAngle);
+            if (movementx != 0)
+            {
+                PlayerAngle = new Vector3(PlayerAngle.x, PlayerAngle.y +180, PlayerAngle.z);
+            }
 
+
+            if (movementz != 0)
+            {
+                PlayerAngle = new Vector3(PlayerAngle.x, PlayerAngle.y +180, PlayerAngle.z);
+            }
+            
+            PlayerTransform.rotation = Quaternion.Euler(PlayerAngle);
+            orientation = -orientation;
             timer -= framerate;
         }
+        
+        
+        transform.position += new Vector3(movementx*orientation*Time.deltaTime, 0f, movementz*orientation*Time.deltaTime);
+        
 
         
         
