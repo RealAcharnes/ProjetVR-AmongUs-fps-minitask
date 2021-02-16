c5fa34df98524dd6784191de7d942fde949822ee
ZXY <46813193+ZhXiangyang@users.noreply.github.com>
2021-02-16 14:22:17 +0100

    bot auto respawn

--- VR project - Copie/Assets/Scripts/BotMovement.cs
+++ VR project - Copie/Assets/Scripts/BotMovement.cs
@@ -9,19 +9,24 @@ public class BotMovement : MonoBehaviour
     private Transform PlayerTransform;
     [SerializeField] private GameObject player;
     private Vector3 PlayerAngle;
+    private float respawnTime;
+    private float respawnTimer;
+    private bool isDead;
     void Start()
     {
         timer = 0.0f;
+        respawnTimer = 0.0f;
+        respawnTime = 3.0f;
         Vector3 playerPosition = transform.position;
         movement = 0.05f;
         PlayerAngle = transform.rotation.eulerAngles;
         PlayerTransform = player.GetComponent<Transform>();
-
+        isDead = false;
     }
     private void Update()
     {
         timer += Time.deltaTime;
-        Debug.Log(timer);
+        //Debug.Log(timer);
         
         
         if (timer >= framerate)
@@ -32,10 +37,22 @@ public class BotMovement : MonoBehaviour
             timer -= framerate;
         }
 
+        if (isDead)
+            respawnTimer += Time.deltaTime;
         
-        
-        
+        if (respawnTimer >= respawnTime)
+        {
+            Instantiate(player);
+            isDead = false;
+            respawnTimer -= respawnTime;
+        }
+
         //transform.position += new Vector3(0, 0, orientation*movement);
-        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(player);
+        isDead = true;
     }
 }
